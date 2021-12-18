ef487324fcc2c9057fa29b78542be71dee6ff4c8
Murilo Ângelo <muriloangelo10@gmail.com>
2021-12-17 22:54:50 -0300

    improving code

--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -1,21 +1,24 @@
 require 'test_helper'
 
 class GameTest < ActiveSupport::TestCase
-  context "A report game" do
-    setup do
-      @game = FactoryBot.create(:game, name: "Infinity Pay Game")
-      player1 = FactoryBot.create(:player, name: "Thiago", game_id: @game.id)
-      player2 = FactoryBot.create(:player, name: "Scalone", game_id: @game.id)
-      player3 = FactoryBot.create(:player, name: "Murilo", game_id: @game.id)
-      FactoryBot.create(:death, assassin_id: player1.id, victim_id: player2.id, game_id: @game.id)
-      FactoryBot.create(:death, assassin_id: player1.id, victim_id: player3.id, game_id: @game.id)
-      FactoryBot.create(:death, assassin_id: player2.id, victim_id: player3.id, game_id: @game.id)
-      FactoryBot.create(:death, assassin_id: nil, victim_id: player2.id, game_id: @game.id, cause: "MOD_FALLING")
-      FactoryBot.create(:death, assassin_id: nil, victim_id: player3.id, game_id: @game.id, cause: "MOD_FALLING")
+  setup do
+    @game = FactoryBot.create(:game, name: "Infinity Pay Game")
+    player1 = FactoryBot.create(:player, name: "Thiago", game_id: @game.id)
+    player2 = FactoryBot.create(:player, name: "Scalone", game_id: @game.id)
+    player3 = FactoryBot.create(:player, name: "Murilo", game_id: @game.id)
+    FactoryBot.create(:death, assassin_id: player1.id, victim_id: player2.id, game_id: @game.id)
+    FactoryBot.create(:death, assassin_id: player1.id, victim_id: player3.id, game_id: @game.id)
+    FactoryBot.create(:death, assassin_id: player2.id, victim_id: player3.id, game_id: @game.id)
+    FactoryBot.create(:death, assassin_id: nil, victim_id: player2.id, game_id: @game.id)
+    FactoryBot.create(:death, assassin_id: nil, victim_id: player2.id, game_id: @game.id, cause: "MOD_FALLING")
+    FactoryBot.create(:death, assassin_id: nil, victim_id: player3.id, game_id: @game.id, cause: "MOD_FALLING")
+  end
 
+  context "should return" do
+    setup do
       @report_response = {
         name: "Infinity Pay Game",
-        total_kills: 5,
+        total_kills: 6,
         players: ["Thiago", "Scalone", "Murilo"],
         kills: [
           {"Thiago": 2},
@@ -25,31 +28,20 @@ class GameTest < ActiveSupport::TestCase
       }
     end
 
-    it "report the game name" do
+    it "report game" do
       assert_equal @game.report, @report_response
     end
   end
 
-  context "A report game kills_by_means" do
+  context "should return" do
     setup do
-      @game = FactoryBot.create(:game, name: "Infinity Pay Game")
-      player1 = FactoryBot.create(:player, name: "Thiago", game_id: @game.id)
-      player2 = FactoryBot.create(:player, name: "Scalone", game_id: @game.id)
-      player3 = FactoryBot.create(:player, name: "Murilo", game_id: @game.id)
-      FactoryBot.create(:death, assassin_id: player1.id, victim_id: player2.id, game_id: @game.id)
-      FactoryBot.create(:death, assassin_id: player1.id, victim_id: player3.id, game_id: @game.id)
-      FactoryBot.create(:death, assassin_id: player2.id, victim_id: player3.id, game_id: @game.id)
-      FactoryBot.create(:death, assassin_id: nil, victim_id: player2.id, game_id: @game.id)
-      FactoryBot.create(:death, assassin_id: nil, victim_id: player2.id, game_id: @game.id, cause: "MOD_FALLING")
-      FactoryBot.create(:death, assassin_id: nil, victim_id: player3.id, game_id: @game.id, cause: "MOD_FALLING")
-
       @report_response = [
         {"MOD_ROCKET": 4},
         {"MOD_FALLING": 2},
       ]
     end
 
-    it "report the game name" do
+    it "report kills_by_means" do
       assert_equal @game.kills_by_means, @report_response
     end
   end
